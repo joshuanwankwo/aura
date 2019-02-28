22cde62739cbfca6059a2e52db3df25a9129c9f6
Joshua <36573481+Systemblack007@users.noreply.github.com>
2019-02-28 22:08:11 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+//like buuton
+var like = document.getElementsByClassName('like-btn');
+
+btn.addEventListener('click', function(){
+
+var calc = document.getElementById('like' ++)
+
+
+document.getElementById('like').value= calc;
+
+
+
+
+
+})
